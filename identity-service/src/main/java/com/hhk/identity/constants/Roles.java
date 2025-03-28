26897ed33e3a4adc7c838093e82683c03170dfa4
huyhuynh12hhk <huyhuynh12.hh@gmail.com>
2025-03-28 10:02:34 +0700

    refactor identity service

--- identity-service/src/main/java/com/hhk/identity/constants/Roles.java
+++
@@ -1,8 +0,0 @@
-package com.hhk.identity.constants;
-
-public class Roles {
-    public static final String USER_ROLE = "USER";
-    public static final String ADMIN_ROLE = "ADMIN";
-
-    private Roles() {}
-}
