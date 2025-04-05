235cbee19abe284af1527c04a2499b97f767d3fb
huyhuynh12hhk <huyhuynh12.hh@gmail.com>
2025-04-05 23:27:15 +0700

    update new auth mechanic: keycloak

--- identity-service/identity-domain/src/main/java/shared/hub/domain/model/constant/Roles.java
+++
@@ -1,8 +0,0 @@
-package shared.hub.domain.model.constant;
-
-public class Roles {
-    public static final String USER_ROLE = "USER";
-    public static final String ADMIN_ROLE = "ADMIN";
-
-    private Roles() {}
-}
